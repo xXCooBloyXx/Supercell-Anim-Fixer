212f961f402b6176a5950ac1504dc308b1c6704f
xXCooBloyXx <89714046+xXCooBloyXx@users.noreply.github.com>
2023-02-12 22:34:49 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+
+folder_path = './input'
+
+if not os.path.exists('input'):
+	os.makedirs('input')
+
+glb_files = []
+for file in os.listdir(folder_path):
+    if file.endswith(".glb") and "_geo.glb" not in file:
+        glb_files.append(file)
+
+for glb_file in glb_files:
+	file_path = os.path.join(folder_path, glb_file)
+	try:
+		with open(file_path, "rb") as f:
+			data = f.read()
+		data = data.replace(b'"mesh"', b'"shem"')
+		with open(file_path, "wb") as f:
+			try:
+				f.write(data)
+				print(f"Succesfully fixed {glb_file}!")
+			except Exception as e1:
+				print(f"Can't save {glb_file}! Error: {e1}")
+				quit()
+	except Exception as e2:
+		print(f"Can't fix {glb_file}! Error: {e2}")
+print("Done!")
+os.system("pause")
